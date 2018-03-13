22e3fd2e8fb18685665c6513444afd1122a5e0aa
Nico Zhu <v-zhumin@microsoft.com>
2018-03-13 15:57:53 +0800

    Add project files.

---
+++ ScroTest/MyListView.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace ScroTest
+{
+    public class MyListView : ListView
+    {
+        private ScrollViewer _scrollViewer;
+
+        public MyListView()
+        {
+            this.Loaded += MyListView_Loaded;
+        }
+
+        private void MyListView_Loaded(object sender, RoutedEventArgs e)
+        {
+            _scrollViewer = (sender as ListView).GetScrollViewer();
+            _scrollViewer.ViewChanged += _scrollViewer_ViewChanged;
+        }
+
+        private void _scrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            var s = default(string);
+
+            var MyScrollViewer = sender as ScrollViewer;
+            this.SetValue(MyPropertyProperty, MyScrollViewer.VerticalOffset);
+        }
+
+        public double PointOffSetY
+        {
+            get { return (double)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("PointOffSetY", typeof(double), typeof(MyListView), new PropertyMetadata(0.0, CallBack));
+
+        private static void CallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var listview = d as ListView;
+            var temScrollViewer = listview.GetScrollViewer();
+            if (e.NewValue != e.OldValue & (double)e.NewValue != 0)
+            {
+                temScrollViewer.ScrollToVerticalOffset((double)e.NewValue);
+            }
+        }
+    }
+
+    public static class Helper
+    {
+        public static ScrollViewer GetScrollViewer(this DependencyObject element)
+        {
+            if (element is ScrollViewer)
+            {
+                return (ScrollViewer)element;
+            }
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i);
+
+                var result = GetScrollViewer(child);
+                if (result == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ ScroTest/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScroTest
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ViewModel()
+        {
+        }
+
+        private double pointY;
+
+        public double PointY
+        {
+            get
+            {
+                return pointY;
+            }
+
+            set
+            {
+                pointY = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
