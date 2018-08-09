8d269ef690cd9d43fd711a2f787691d4fc0111b4
Nico Zhu <v-zhumin@microsoft.com>
2018-08-09 17:43:17 +0800

    add attach property

--- ScroTest/MyListView.cs
+++ ScroTest/MyListView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xaml.Interactivity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,110 @@ using Windows.UI.Xaml.Media;
 
 namespace ScroTest
 {
+    public abstract class Behavior<T> : Behavior where T : DependencyObject
+    {
+        protected T AssociatedObject
+        {
+            get { return base.AssociatedObject as T; }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if (this.AssociatedObject == null) throw new InvalidOperationException("AssociatedObject is not of the right type");
+        }
+    }
+
+    public abstract class Behavior : DependencyObject, IBehavior
+    {
+        public void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject;
+            OnAttached();
+        }
+
+        public void Detach()
+        {
+            OnDetaching();
+        }
+
+        protected virtual void OnAttached() { }
+
+        protected virtual void OnDetaching() { }
+
+        protected DependencyObject AssociatedObject { get; set; }
+
+        DependencyObject IBehavior.AssociatedObject
+        {
+            get { return this.AssociatedObject; }
+        }
+    }
+
+    public class HtmlTextBehavior : Behavior<ScrollViewer>
+    {
+        private static ScrollViewer _scrollViewer;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.Loaded += OnAssociatedObjectLoaded;
+            AssociatedObject.LayoutUpdated += OnAssociatedObjectLayoutUpdated;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.Loaded -= OnAssociatedObjectLoaded;
+            AssociatedObject.LayoutUpdated -= OnAssociatedObjectLayoutUpdated;
+        }
+
+        private void OnAssociatedObjectLayoutUpdated(object sender, object o)
+        {
+            UpdateText();
+        }
+
+        private void OnAssociatedObjectLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            UpdateText();
+            AssociatedObject.Loaded -= OnAssociatedObjectLoaded;
+        }
+
+        private void UpdateText()
+        {
+            if (AssociatedObject == null) return;
+
+            AssociatedObject.ViewChanged += AssociatedObject_ViewChanged;
+        }
+
+        private void AssociatedObject_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            var MyScrollViewer = sender as ScrollViewer;
+            this.SetValue(PointOffSetYProperty, MyScrollViewer.VerticalOffset);
+
+        }
+
+        public double PointOffSetY
+        {
+            get { return (double)GetValue(PointOffSetYProperty); }
+            set { SetValue(PointOffSetYProperty, value); }
+        }
+     
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PointOffSetYProperty =
+            DependencyProperty.Register("PointOffSetY", typeof(double), typeof(HtmlTextBehavior), new PropertyMetadata(0.0, CallBack));
+
+        private static void CallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var current = d as HtmlTextBehavior;
+            var temScrollViewer = current.AssociatedObject;
+            if (e.NewValue != e.OldValue & (double)e.NewValue != 0)
+            {
+                temScrollViewer.ScrollToVerticalOffset((double)e.NewValue);
+            }
+        }
+
+
+    }
+
     public class MyListView : ListView
     {
         private ScrollViewer _scrollViewer;
@@ -29,17 +134,17 @@ namespace ScroTest
             var s = default(string);
 
             var MyScrollViewer = sender as ScrollViewer;
-            this.SetValue(MyPropertyProperty, MyScrollViewer.VerticalOffset);
+            this.SetValue(PointOffSetYProperty, MyScrollViewer.VerticalOffset);
         }
 
         public double PointOffSetY
         {
-            get { return (double)GetValue(MyPropertyProperty); }
-            set { SetValue(MyPropertyProperty, value); }
+            get { return (double)GetValue(PointOffSetYProperty); }
+            set { SetValue(PointOffSetYProperty, value); }
         }
 
         // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty MyPropertyProperty =
+        public static readonly DependencyProperty PointOffSetYProperty =
             DependencyProperty.Register("PointOffSetY", typeof(double), typeof(MyListView), new PropertyMetadata(0.0, CallBack));
 
         private static void CallBack(DependencyObject d, DependencyPropertyChangedEventArgs e)
